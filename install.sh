9108213de86ee21faaf2e1a02472a7c2d1886254
chino <chino@mescaline.localdomain>
2009-03-02 00:33:38 +0100

    Add a better support for installation scripts tools

--- install.sh
+++ install.sh
@@ -1,33 +1,16 @@
-#! /bin/zsh
+#! /bin/sh
 
-ROUGE="\033[31;01m"
-VERT="\033[32;01m"
-JAUNE="\033[33;01m"
-BLEU="\033[34;01m"
-MAGENTA="\033[35;01m"
-CYAN="\033[36;01m"
-BLANC="\033[37;01m"
-NEUTRE="\033[0m"
+source "../functions/functions"
+RM=rm
 
-echo "Zsh Install..."
+stat_busy "Zsh installation"
 
-rm ~/.zshrc
-ln -s $PWD/zshrc ~/.zshrc
-echo -n "$VERT*$NEUTRE  .zshrc linked\t"
-echo -e "${BLEU}[${VERT}OK${BLEU}]${NEUTRE}"
+for file in zshrc zsh; do
+  if [ -e ~/.$file ]; then
+    $RM ~/.$file
+  fi
+  ln -s $PWD/$file ~/.$file
+  printhl "$file linked"
+done
 
-rm ~/.zsh
-ln -s $PWD/zsh ~/.zsh
-echo -n "$VERT*$NEUTRE  .zsh linked\t\t"
-echo -e "${BLEU}[${VERT}OK${BLEU}]${NEUTRE}"
-
-if [ "$SHELL" != "/bin/zsh" ]; then
-  echo -n "$JAUNE*$NEUTRE  Your shell is $SHELL, change it.\t"
-  echo -e "${BLEU}[${VERT}OK${BLEU}]${NEUTRE}"
-else
-  source ~/.zshrc
-  echo -n "$VERT*$NEUTRE  Sourced.\t\t"
-  echo -e "${BLEU}[${VERT}OK${BLEU}]${NEUTRE}"
-fi
-
-echo -e "Done."
+stat_done
